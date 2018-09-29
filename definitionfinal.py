d155581c4f856ae0583e3d0b1b8dd7700f5999ca
AreebaAftab <areebanovember@hotmail.com>
2018-09-30 00:27:49 +0500

    semantic analysis

---
+++ definitionfinal.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 00:26:22 2018
+
+@author: Areeba aftab
+"""
+
+import gensim
+import os
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
+from nltk.corpus import wordnet as wn 
+#from checkwindow import Ui_checkwindow
+ 
+       
+    
